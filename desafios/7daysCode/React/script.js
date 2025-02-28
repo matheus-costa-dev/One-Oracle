cb9b71267cb92795b85f07b43a3ba9b071116081
matheus costa <dev.matheuspc@gmail.com>
2025-02-28 19:56:25 -0300

    desafio html feito

---
+++ desafios/7daysCode/React/script.js
@@ -0,0 +1,8 @@
+function inscrever_newsletter(){
+    const email = document.querySelector(".container__email").value
+
+    if(email != "" && email != null){
+
+        console.log(email)
+    }
+}
